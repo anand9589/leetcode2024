0cd35963b3794c689f7bde8ebfa173bc374f22af
anand9589 <anand9589@gmail.com>
2024-01-01 12:15:15 -0600

    Added Assign Cookies Problme

--- January/January.cs
+++ January/January.cs
@@ -1,7 +1,31 @@
 ﻿namespace January
 {
-    public class Class1
+    public class January
     {
+        #region Day 1 455. Assign Cookies
+
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Sort(s);
+
+            int i = 0;
+            int j = 0;
+            int contentChildren = 0;
+            while (i < g.Length && j < s.Length)
+            {
+                if (s[j] >= g[i])
+                {
+                    contentChildren++;
+                    i++;
+                }
+                j++;
+
+            }
+            return contentChildren;
+        }
+
+        #endregion
 
     }
 }
