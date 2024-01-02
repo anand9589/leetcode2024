3f78364e3eecf9b9673eb80c21101e2ac30001e8
anand9589 <anand9589@gmail.com>
2024-01-02 17:19:04 -0600

    Added 2610. Convert an Array Into a 2D Array With Conditions

--- January/January.cs
+++ January/January.cs
@@ -27,5 +27,45 @@
 
         #endregion
 
+        #region Day 2 2610. Convert an Array Into a 2D Array With Conditions
+        public IList<IList<int>> FindMatrix(int[] nums)
+        {
+            IList<IList<int>> ls = new List<IList<int>>();
+
+            Dictionary<int, int> dct = new Dictionary<int, int>();
+            foreach (int i in nums)
+            {
+                if (!dct.ContainsKey(i))
+                {
+                    dct.Add(i, 1);
+                }
+                else
+                {
+                    dct[i]++;
+                }
+            }
+
+            dct = dct.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            int k1 = dct.Keys.First();
+            int v1 = dct.Values.First();
+            for (int i = 0; i < v1; i++)
+            {
+                ls.Add(new List<int>() { k1 });
+            }
+            dct.Remove(k1);
+
+            foreach (var key in dct.Keys)
+            {
+                for (int i = 0; i < dct[key]; i++)
+                {
+                    ls[i].Add(key);
+                }
+            }
+
+            return ls;
+        }
+        #endregion
+
     }
 }
