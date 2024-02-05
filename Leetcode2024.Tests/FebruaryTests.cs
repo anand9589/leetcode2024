7b70ab029e1cf54fb26f3a5cbeb52d5bdce748bc
anand9589 <anand9589@gmail.com>
2024-02-04 19:29:36 -0600

    Added 143. Reorder List

---
+++ Leetcode2024.Tests/FebruaryTests.cs
@@ -0,0 +1,31 @@
+﻿using Leetcode2024.Common.Models;
+
+namespace Leetcode2024.Tests
+{
+    internal class FebruaryTests
+    {
+        February february;
+        [SetUp]
+        public void Setup()
+        {
+             february = new February();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            int[] arr = new int[] {1, 2, 3, 4};
+
+            ListNode listNode = new ListNode();
+            ListNode temp = listNode;
+
+            foreach (var item in arr)
+            {
+                temp.next = new ListNode(item);
+                temp = temp.next;
+            }
+
+            february.ReorderList(listNode.next);
+        }
+    }
+}
--- Leetcode2024/February.cs
+++ Leetcode2024/February.cs
@@ -1,4 +1,6 @@
-﻿namespace Leetcode2024
+﻿using Leetcode2024.Common.Models;
+
+namespace Leetcode2024
 {
     public class February
     {
@@ -22,6 +24,53 @@
             }
 
             return lst.ToArray();
+        }
+        #endregion
+
+        #region 143. Reorder List
+
+        public void ReorderList(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next; fast = fast.next.next;
+            }
+
+            ListNode second = reverseList(slow.next);
+
+            slow.next = null;
+            ListNode first = head;
+
+            while (second!=null)
+            {
+                ListNode temp1 = first.next;
+                ListNode temp2 = second.next;
+                first.next = second;
+                second.next = temp1;
+                first = temp1;
+                second = temp2;
+            }
+        }
+
+
+        private ListNode reverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode current = head;
+            ListNode next = null;
+
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+
+            return prev;
         }
         #endregion
     }
