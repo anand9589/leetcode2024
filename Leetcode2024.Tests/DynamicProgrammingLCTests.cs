ac6688373d49b7ef90993b3f456c15dedbe8f67a
anand9589 <anand9589@gmail.com>
2024-11-01 17:06:22 -0400

    Added 70. Climbing Stairs

---
+++ Leetcode2024.Tests/DynamicProgrammingLCTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Leetcode2024.Tests
+{
+    internal class DynamicProgrammingLCTests
+    {
+        DynamicProgrammingLC dp;
+        [SetUp]
+        public void Setup()
+        {
+            dp = new DynamicProgrammingLC();
+        }
+
+        [Test]
+        public void ClimbStairsTest1()
+        {
+            int res = dp.ClimbStairs(3);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ Leetcode2024/DynamicProgrammingLC.cs
@@ -0,0 +1,31 @@
+﻿namespace Leetcode2024
+{
+    public class DynamicProgrammingLC
+    {
+        #region 70. Climbing Stairs
+        public int ClimbStairs_Recursion(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+
+            return ClimbStairs_Recursion(n - 1) + ClimbStairs_Recursion(n - 2);
+        }
+
+        public int ClimbStairs(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+            int[] dp = new int[n];
+            dp[0] = 1;
+            dp[1] = 2;
+
+            for (int i = 2; i < n; i++)
+            {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+
+            return dp[n - 1];
+        }
+        #endregion
+    }
+}
