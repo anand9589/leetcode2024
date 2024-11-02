0095ad5da03e6a44a5161d227607d06b223f40d1
anand9589 <anand9589@gmail.com>
2024-11-01 22:47:34 -0400

    Added 740. Delete and Earn

--- Leetcode2024.Tests/DynamicProgrammingLCTests.cs
+++ Leetcode2024.Tests/DynamicProgrammingLCTests.cs
@@ -15,5 +15,33 @@
             int res = dp.ClimbStairs(3);
             Assert.AreEqual(3, res);
         }
+
+        [Test]
+        public void DeleteAndEarnTest1()
+        {
+            int res = dp.DeleteAndEarn(new int[] { 3, 4, 2 });
+            Assert.AreEqual(6, res);
+        }
+
+        [Test]
+        public void DeleteAndEarnTest2()
+        {
+            int res = dp.DeleteAndEarn(new int[] { 2, 2, 3, 3, 3, 4 });
+            Assert.AreEqual(9, res);
+        }
+
+        [Test]
+        public void DeleteAndEarnTest3()
+        {
+            int res = dp.DeleteAndEarn(new int[] { 1, 1, 1, 2, 4, 5, 5, 5, 6 });
+            Assert.AreEqual(18, res);
+        }
+
+        [Test]
+        public void DeleteAndEarnTest4()
+        {
+            int res = dp.DeleteAndEarn(new int[] { 1, 6, 3, 3, 8, 4, 8, 10, 1, 3 });
+            Assert.AreEqual(43, res);
+        }
     }
 }
--- Leetcode2024/DynamicProgrammingLC.cs
+++ Leetcode2024/DynamicProgrammingLC.cs
@@ -1,4 +1,6 @@
-﻿namespace Leetcode2024
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Leetcode2024
 {
     public class DynamicProgrammingLC
     {
@@ -51,6 +53,141 @@
         }
 
 
+        #endregion
+
+        #region 740. Delete and Earn
+
+        public int DeleteAndEarn(int[] nums)
+        {
+            int maxNum = Max(nums);
+
+            int[] dp1 = new int[(int)maxNum + 1];
+
+            foreach (int n in nums) dp1[n] += n;
+
+            int[] dp = new int[(int)maxNum + 1];
+            dp[1] = dp1[1];
+
+            for (int i = 2; i <= maxNum; i++)
+            {
+                dp[i] = Math.Max(dp[i - 1], dp[i - 2] + dp1[i]);
+            }
+
+            return dp[maxNum];
+        }
+
+        private int Max(int[] nums)
+        {
+            int maxNum = 0;
+            foreach (var item in nums)
+            {
+                if (item > maxNum)
+                {
+                   maxNum =  item;
+                }
+            }
+            return maxNum;
+        }
+
+        public int DeleteAndEarn_V2(int[] nums)
+        {
+            Array.Sort(nums);
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            foreach (int n in nums)
+            {
+                if (!map.ContainsKey(n))
+                {
+                    map.Add(n, 0);
+                }
+                map[n] += n;
+            }
+
+            int prev1 = 0;
+
+            int[] keys = map.Keys.ToArray();
+            int[] values = map.Values.ToArray();
+            int prev2 = values[0];
+            for (int i = 1; i < map.Keys.Count; i++)
+            {
+                int currEarn = values[i];
+                int temp;
+                if (keys[i] == keys[i - 1] + 1)
+                {
+                    temp = prev2;
+                    prev2 = Math.Max(currEarn + prev1, prev2);
+                    prev1 = temp;
+                }
+                else
+                {
+                    temp = prev2;
+                    prev2 = currEarn + prev2;
+                    prev1 = temp;
+                }
+
+            }
+            return prev2;
+        }
+        public int DeleteAndEarn_v1(int[] nums)
+        {
+            SortedDictionary<int, int> map = new SortedDictionary<int, int>();
+
+            foreach (int n in nums)
+            {
+                if (!map.ContainsKey(n))
+                {
+                    map.Add(n, 0);
+                }
+                map[n] += n;
+            }
+
+            if (map.Count == 1) return map.Values.First();
+
+            int[] dp = new int[map.Count];
+
+
+            int[] keys = map.Keys.ToArray();
+            int[] values = map.Values.ToArray();
+
+            dp[0] = values[0];
+            dp[1] = values[1];
+
+            if (keys[1] - keys[0] == 1) { dp[1] = Math.Max(dp[1], dp[0]); }
+            else dp[1] += dp[0];
+
+            if (map.Count > 2)
+            {
+                dp[2] = values[2];
+
+                if (keys[2] - keys[1] == 1)
+                {
+                    dp[2] += dp[0];
+                    dp[2] = Math.Max(dp[2], dp[1]);
+                }
+                else dp[2] += dp[1];
+
+
+            }
+
+
+            for (int i = 3; i < map.Count; i++)
+            {
+                dp[i] = values[i];
+
+                if (keys[i] - keys[i - 1] == 1)
+                {
+                    dp[i] += dp[i - 2];
+                    dp[i] = Math.Max(dp[i], dp[i - 2]);
+                }
+                else
+                {
+                    dp[i] += dp[i - 1];
+                }
+            }
+
+            return Math.Max(dp[map.Count - 1], dp[map.Count - 2]);
+
+        }
         #endregion
 
         #region 746. Min Cost Climbing Stairs
