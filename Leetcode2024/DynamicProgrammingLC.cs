380e779287eab714bbc552c941d31f92e6fc9c1a
anand9589 <anand9589@gmail.com>
2024-11-02 00:13:03 -0400

    63. Unique Paths II

--- Leetcode2024/DynamicProgrammingLC.cs
+++ Leetcode2024/DynamicProgrammingLC.cs
@@ -49,6 +49,65 @@
         }
         #endregion
 
+        #region 63. Unique Paths II
+        public int UniquePathsWithObstacles(int[][] obstacleGrid)
+        {
+            int m = obstacleGrid.Length;
+            int n = obstacleGrid[0].Length;
+
+            if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) return 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (obstacleGrid[i][j] == 1)
+                    {
+                        obstacleGrid[i][j] = -1;
+                        continue;
+                    }
+
+                    if (i == 0 && j == 0) { obstacleGrid[i][j] = 1; continue; }
+
+                    if (i == 0)
+                    {
+                        obstacleGrid[i][j] = obstacleGrid[i - 1][j] == -1 ? -1 : 1;
+                        continue;
+                    }
+
+                    if (j == 0)
+                    {
+                        obstacleGrid[i][j] = obstacleGrid[i][j - 1] == -1 ? -1 : 1;
+                        continue;
+                    }
+
+                    if (obstacleGrid[i - 1][j] == -1 && obstacleGrid[i][j - 1] == -1)
+                    {
+                        obstacleGrid[i][j] = -1;
+                        continue;
+                    }
+
+                    if (obstacleGrid[i - 1][j] == -1)
+                    {
+                        obstacleGrid[i][j] = obstacleGrid[i][j - 1];
+                        continue;
+                    }
+
+
+                    if (obstacleGrid[i][j - 1] == -1)
+                    {
+                        obstacleGrid[i][j] = obstacleGrid[i - 1][j];
+                        continue;
+                    }
+
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+                }
+            }
+
+            return obstacleGrid[m - 1][n - 1] == -1 ? 0 : obstacleGrid[m - 1][n - 1];
+        }
+        #endregion
+
         #region 64. Minimum Path Sum
         public int MinPathSum(int[][] grid)
         {
