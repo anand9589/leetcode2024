d96b3e5fecdd424e312bb370a525635ec34e9bc5
anand9589 <anand9589@gmail.com>
2024-11-07 19:56:05 -0500

    Added HackRank

---
+++ HackerRank/HackerRank.cs
@@ -0,0 +1,36 @@
+﻿namespace HackerRank
+{
+    public class HackerRank
+    {
+        public static List<int> matchingStrings(List<string> stringList, List<string> queries)
+        {
+            Dictionary<string, int> map = new Dictionary<string, int>();
+            List<int> result = new List<int>();
+            foreach (string s in stringList)
+            {
+                if (!map.ContainsKey(s))
+                {
+                    map.Add(s, 1);
+                }
+                else
+                {
+                    map[s]++;
+                }
+            }
+
+            foreach (var q in queries)
+            {
+                if (map.ContainsKey(q))
+                {
+                    result.Add(map[q]);
+                }
+                else
+                {
+                    result.Add(0);
+                }
+            }
+
+            return result;
+        }
+    }
+}
